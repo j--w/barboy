c0d38b456c50fd02144bc52c7fb5f96ca0aff3b9
Jim Wright <jw@jimbo.io>
2018-07-29 21:52:37 +0100

    remove old hard coded interfaces and post-merge hook

--- client/src/shared-interfaces/BarState.ts
+++
@@ -1,6 +0,0 @@
-import Pump from '@/shared-interfaces/Pump';
-
-export default interface BarState {
-    isBusy: boolean;
-    pumps: Pump[];
-}
--- client/src/shared-interfaces/Ingredient.ts
+++
@@ -1,6 +0,0 @@
-export default interface Ingredient {
-    label: string;
-    alcoholByVolume: number;
-    brand?: string;
-    sku?: string;
-}
